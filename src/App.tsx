f722ab823263b2fcd3c447be1c7c344f96291c2b
karolhas <karolekhas@gmail.com>
2023-12-12 20:28:21 +0100

    Feat: Add style + connect API + change title & meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useRef, useState } from "react";
+import { youtubeParser } from "./utils";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const inputUrlRef = useRef<HTMLInputElement | null>(null);
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const youtubeID = youtubeParser(inputUrlRef.current?.value || "");
+    // console.log(youtubeID);
+
+    const options = {
+      method: "GET",
+      url: "https://youtube-mp36.p.rapidapi.com/dl",
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_KEY,
+        "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
+      },
+      params: {
+        id: youtubeID,
+      },
+    };
+
+    axios(options)
+      .then((res) => setUrlResult(res.data.link))
+      .catch((err) => console.log(err));
+
+    if (inputUrlRef.current) {
+      inputUrlRef.current.value = "";
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      <span className="logo">YTtoMP3</span>
 
-export default App
+      <section className="content">
+        <h1 className="content_title">YouTube to MP3 Converter</h1>
+        <p className="content_description">
+          Transform YouTube videos into MP3s in just few clicks!
+        </p>
+
+        <form onSubmit={handleSubmit} className="form">
+          <input
+            ref={inputUrlRef}
+            className="form_input"
+            placeholder="Paste a YouTube video URL link here..."
+            type="text"
+          />
+          <button type="submit" className="form_button">
+            Search
+          </button>
+        </form>
+
+        {urlResult ? (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={urlResult}
+            className="download_btn"
+          >
+            Download MP3
+          </a>
+        ) : (
+          ""
+        )}
+      </section>
+    </div>
+  );
+}
---
+++ src/utils.tsx
@@ -0,0 +1,6 @@
+export function youtubeParser(url: string): string | false {
+  const regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length === 11 ? match[7] : false;
+}
